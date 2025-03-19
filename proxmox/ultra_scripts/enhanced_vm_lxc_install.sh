1c7bb50f82fb0cea9c51120e6c61d369f9a39672
CIIO <g@clarkind.net>
2025-03-18 18:00:06 -0700

    - copilot?

--- proxmox/ultra_scripts/enhanced_vm_lxc_install.sh
+++ proxmox/ultra_scripts/enhanced_vm_lxc_install.sh
@@ -42,9 +42,11 @@ if [[ $? -ne 0 ]]; then exit 1; fi
 # Select Storage
 STORAGE_OPTIONS=$(pvesm status | awk 'NR>1 {print $1}')
 DEFAULT_STORAGE=$(echo "$STORAGE_OPTIONS" | awk '{print $1}')
-STORAGE_SELECTION="" for s in $storage_options; do
-STORAGE_SELECTION+="$s Storage  " done
-STORAGE=$(whiptail --title "Select Storage" --menu \ "Choose where to store the LXC container:" 20 60 10 \ $STORAGE_SELECTION 3>&1 1>&2 2>&3)
+STORAGE_SELECTION=""
+for s in $STORAGE_OPTIONS; do
+    STORAGE_SELECTION+="$s Storage  "
+done
+STORAGE=$(whiptail --title "Select Storage" --menu "Choose where to store the LXC container:" 20 60 10 $STORAGE_SELECTION 3>&1 1>&2 2>&3)
 if [[ $? -ne 0 ]]; then exit 1; fi
 
 # Network Configuration
