52525a1f534cdd8a624a202242a83f4155759a72
CIIO <g@clarkind.net>
2025-03-18 17:56:14 -0700

    - getting there

--- proxmox/ultra_scripts/enhanced_vm_lxc_install.sh
+++ proxmox/ultra_scripts/enhanced_vm_lxc_install.sh
@@ -42,10 +42,7 @@ if [[ $? -ne 0 ]]; then exit 1; fi
 # Select Storage
 STORAGE_OPTIONS=$(pvesm status | awk 'NR>1 {print $1}' | xargs)
 DEFAULT_STORAGE=$(echo "$STORAGE_OPTIONS" | awk '{print $1}')
-STORAGE_SELECTION=""
-for s in $storage_options; do
-    STORAGE_SELECTION+="$s Storage  "  # Correctly format without "x"
-done
+STORAGE_SELECTION="" for s in $storage_options; do STORAGE_SELECTION+="$s Storage  " done
 STORAGE=$(whiptail --title "Select Storage" --menu \ "Choose where to store the LXC container:" 20 60 10 \ $STORAGE_SELECTION 3>&1 1>&2 2>&3)
 if [[ $? -ne 0 ]]; then exit 1; fi
 
